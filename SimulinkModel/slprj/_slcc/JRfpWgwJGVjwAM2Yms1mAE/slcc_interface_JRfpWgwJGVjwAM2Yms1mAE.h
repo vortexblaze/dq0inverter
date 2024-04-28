696de860890f2ddaff729c103a96e86a6749e519
vortexblaze <impulsegundamms@gmail.com>
2024-04-28 23:19:35 +0800

    modified:   Log.txt 	modified:   Referencegentest.slx 	deleted:    Referencegentest.slx.autosave 	modified:   SimulinkModel/dq0Invertermeow.slx 	modified:   SimulinkModel/dq0inverter_DDSRF.slx

---
+++ SimulinkModel/slprj/_slcc/JRfpWgwJGVjwAM2Yms1mAE/slcc_interface_JRfpWgwJGVjwAM2Yms1mAE.h
@@ -0,0 +1,28 @@
+#include "customcode_JRfpWgwJGVjwAM2Yms1mAE.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Type Definitions */
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+
+/* Function Declarations */
+DLL_EXPORT_CC extern const char_T *get_dll_checksum_JRfpWgwJGVjwAM2Yms1mAE();
+DLL_EXPORT_CC extern real_T getInPowerValue_JRfpWgwJGVjwAM2Yms1mAE(real_T Iin, real_T Vin);
+DLL_EXPORT_CC extern real_T getOutPowerValue_JRfpWgwJGVjwAM2Yms1mAE(real_T Iout, real_T Vout);
+DLL_EXPORT_CC extern real_T PowerIntegral_JRfpWgwJGVjwAM2Yms1mAE(real_T Time, real_T currentPin, real_T currentPout);
+DLL_EXPORT_CC extern real_T IdrefCalc_JRfpWgwJGVjwAM2Yms1mAE(real_T Time, real_T Vd, real_T Iin, real_T Iout, real_T Vin, real_T Vout);
+DLL_EXPORT_CC extern boolean_T ReferenceGenTrigger_JRfpWgwJGVjwAM2Yms1mAE(real_T freq);
+
+/* Function Definitions */
+DLL_EXPORT_CC const uint8_T *get_checksum_source_info(int32_T *size);
+#ifdef __cplusplus
+}
+#endif
+
